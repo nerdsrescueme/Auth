79f26165dc2efd3642050b2d72c5cec177f7d24b
Frank Bardon Jr <frank@nerdsrescue.me>
2012-10-19 11:03:24 -0400

    Added abstract user models to support authentication

---
+++ Auth/Model/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Auth\Model;
+
+abstract class User extends \Nerd\Model
+{
+    protected static $table = 'nerd_users';
+
+	public $meta;
+
+	public function __construct()
+	{
+		$meta = get_called_class().'\\Meta';
+		$this->meta = $meta::findOne('SELECT * FROM nerd_user_metadata WHERE user_id = ?', $this->_values['id']);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function __get($property)
+	{
+		if (isset($this->_values[$property])) {
+            return $this->_values[$property];
+        } else {
+			if ($this->meta) {
+				return $this->meta->__get($property);
+			}
+		}
+	}
+}
---
+++ Auth/Model/User/Meta.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Auth\Model\User;
+
+class Meta extends \Nerd\Model
+{
+	protected static $table = 'nerd_user_metadata';
+}
